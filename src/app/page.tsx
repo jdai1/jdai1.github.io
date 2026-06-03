1e268c707a61577a79c278c23fffa162693c79f5
jdai1 <julian.dai@gmail.com>
2026-06-02 22:29:42 -0400

    Update homepage bio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div className="p-4">
       <div className="text-lg text-text-primary space-y-4">
         <h2>Hi, I'm Julian! </h2>
-        <p>I'm currently finishing my last year at Brown University while working as a software engineer at <a href="https://ramp.com" target="_blank" rel="noopener noreferrer">Ramp</a>   to build and scale products in the procure-to-pay space.</p>
+        <p>I'm a software engineer at <a href="https://ramp.com" target="_blank" rel="noopener noreferrer">Ramp</a>, where I build and scale products in the procure-to-pay space.</p>
         <p></p>
         <p>
           Reach out via{" "}
